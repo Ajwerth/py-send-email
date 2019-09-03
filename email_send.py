a3ba113761b93edcfd9e5360fbb34e0a62f25bb4
Alec J. Wertheimer <alec.wertheimer@gmail.com>
2019-09-03 15:29:14 -0400

    added gitignore

--- email_send.py
+++ email_send.py
@@ -1,8 +1,23 @@
 import logging
-from helpers import get_contacts
+import smtplib
+
+from helpers import get_contacts, create_temp_obj
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 # Set loggin level
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
+MY_ADDRESS = 'my_address@example.comm'
+PASSWORD = 'mypassword'
+logging.info(f'SMTP Address set: {MY_ADDRESS}')
+logging.info(f'SMTP Password Set: {PASSWORD}')
+
+# set up the SMTP server
+s = smtplib.SMTP(host='your_host_address_here', port=your_port_here)
+s.starttls()
+s.login(MY_ADDRESS, PASSWORD)
+
+create_temp_obj('message.txt')
 get_contacts('contacts.txt')
